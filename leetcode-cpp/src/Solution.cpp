0a2e8a998a79ef02fced5b894fc79816aa353311
Fizgrad <13272507567@163.com>
2023-03-04 18:58:09 +0800

    982. Triples with Bitwise AND Equal To Zero

--- leetcode-cpp/src/Solution.cpp
+++ leetcode-cpp/src/Solution.cpp
@@ -184,6 +184,16 @@ public:
         }
         return res;
     }
+    int countTriplets(vector<int>& nums) {
+        int cnt = 0;
+        int tuples[1 << 16] = {};
+        for (auto a : nums)
+            for (auto b : nums) ++tuples[a & b];
+        for (auto a : nums)
+            for (auto i = 0; i < (1 << 16); ++i)
+                if ((i & a) == 0) cnt += tuples[i];
+        return cnt;
+    }
 
 };
 
