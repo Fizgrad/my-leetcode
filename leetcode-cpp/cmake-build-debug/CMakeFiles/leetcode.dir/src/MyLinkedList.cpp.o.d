7b4e44f1076bce728e435b42d751dffe13278bdb
Fizgrad Chen <68102312+Fizgrad@users.noreply.github.com>
2023-03-25 02:10:18 +0800

    Delete leetcode-cpp/cmake-build-debug directory

--- leetcode-cpp/cmake-build-debug/CMakeFiles/leetcode.dir/src/MyLinkedList.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/leetcode.dir/src/MyLinkedList.cpp.o: \
-  /Users/david/leetcode/leetcode-cpp/src/MyLinkedList.cpp
--- leetcode-cpp/cmake-build-debug/CMakeFiles/leetcode.dir/src/diameterOfBinaryTree.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/leetcode.dir/src/diameterOfBinaryTree.cpp.o: \
-  /Users/david/leetcode/leetcode-cpp/src/diameterOfBinaryTree.cpp
