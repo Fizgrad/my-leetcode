982829a5694f5349d3cf085ecd179a45e48546fa
Fizgrad <13272507567@163.com>
2023-03-28 19:39:12 +0800

    983. Minimum Cost For Tickets

--- leetcode-cpp/src/Solution.cpp
+++ leetcode-cpp/src/Solution.cpp
@@ -2174,6 +2174,22 @@ public:
         }
         return res;
     }
+
+    int mincostTickets(vector<int> &days, vector<int> &costs) {
+        int dp[366] = {0};
+        bool travelDays[366] = {false};
+        for (int day: days) {
+            travelDays[day] = true;
+        }
+        for (int i = 1; i <= 365; i++) {
+            if (!travelDays[i]) {
+                dp[i] = dp[i - 1];
+            } else {
+                dp[i] = min(dp[i - 1] + costs[0], min(dp[max(0, i - 7)] + costs[1], dp[max(0, i - 30)] + costs[2]));
+            }
+        }
+        return dp[365];
+    }
 };
 
 int main() {
