7a5de5db42fd7fd28fec3828b1e526b1b2ee5b3d
Fizgrad <13272507567@163.com>
2023-04-02 08:59:40 +0800

    2013. Detect Squares

---
+++ leetcode-cpp/src/DetectSquares.cpp
@@ -0,0 +1,42 @@
+//
+// Created by David Chen on 4/2/23.
+//
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class DetectSquares {
+public:
+    vector<unordered_map<int, int>> board;
+
+    DetectSquares() : board(vector<unordered_map<int, int>>(1001)) {
+    }
+
+    void add(vector<int> point) {
+        ++board[point[0]][point[1]];
+    }
+
+    int count(vector<int> point) {
+        int res = 0;
+        for (auto i = 0; i < 1001; ++i) {
+            if (board[i][point[1]] > 0) {
+                int len = abs(i - point[0]);
+                if (len > 0) {
+                    res += board[i][point[1] + len] * board[i][point[1]] *
+                           board[point[0]][point[1] + len];
+                    res += board[i][point[1] - len] * board[i][point[1]] *
+                           board[point[0]][point[1] - len];
+                }
+            }
+        }
+        return res;
+    }
+};
+
+/**
+ * Your DetectSquares object will be instantiated and called as such:
+ * DetectSquares* obj = new DetectSquares();
+ * obj->add(point);
+ * int param_2 = obj->count(point);
+ */
