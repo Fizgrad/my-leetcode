c6d09e5eac2c850148395be22dda93b69eba7bec
Fizgrad <13272507567@163.com>
2023-04-04 02:01:49 +0800

    950. Reveal Cards In Increasing Order

--- leetcode-cpp/src/Solution.cpp
+++ leetcode-cpp/src/Solution.cpp
@@ -2598,6 +2598,24 @@ public:
         }
         return res->next;
     }
+
+    vector<int> deckRevealedIncreasing(vector<int> &deck) {
+        sort(deck.begin(), deck.end());
+        if (deck.size() <= 2) {
+            return deck;
+        }
+        deque<int> ans;
+        ans.push_front(deck.back());
+        deck.pop_back();
+        while (!deck.empty()) {
+            int temp = ans.back();
+            ans.pop_back();
+            ans.push_front(temp);
+            ans.push_front(deck.back());
+            deck.pop_back();
+        }
+        return std::move(vector<int>(ans.begin(), ans.end()));
+    }
 };
 
 int main() {
