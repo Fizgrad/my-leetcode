b1cb97911a736b41212095436f8c0888541ec5ae
Fizgrad <13272507567@163.com>
2023-04-14 14:32:21 +0800

    516. Longest Palindromic Subsequence

--- leetcode-cpp/src/Solution.cpp
+++ leetcode-cpp/src/Solution.cpp
@@ -3043,6 +3043,24 @@ public:
         }
         return true;
     }
+
+    int longestPalindromeSubseq(const string &s) {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for (auto i = 0; i < n; ++i) {
+            dp[i][i] = 1;
+        }
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                dp[i][j] = max(dp[i][j - 1], dp[i + 1][j]);
+                if (s[i] == s[j]) {
+                    dp[i][j] = max(dp[i][j], 2 + dp[i + 1][j - 1]);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+
 };
 
 int main() {
