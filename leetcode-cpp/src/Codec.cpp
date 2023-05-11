f6f38cfd78cfe4c0570d7a5603beda3a06231c8d
Fizgrad <13272507567@163.com>
2023-05-12 02:54:07 +0800

    297. Serialize and Deserialize Binary Tree

--- leetcode-cpp/src/Codec.cpp
+++ leetcode-cpp/src/Codec.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <queue>
 
 using namespace std;
 
@@ -14,18 +15,70 @@ struct TreeNode {
 
 class Codec {
 public:
-
     // Encodes a tree to a single string.
     string serialize(TreeNode *root) {
-        string res;
-        
-
+        string res = "[";
+        queue<TreeNode *> q;
+        q.push(root);
+        while (!q.empty()) {
+            auto i = q.front();
+            q.pop();
+            if (i == nullptr) {
+                res.append("n,");
+            } else {
+                res.append(to_string(i->val));
+                res.append(",");
+                q.push(i->left);
+                q.push(i->right);
+            }
+        }
+        res.pop_back();
+        res.push_back(']');
         return std::move(res);
     }
 
     // Decodes your encoded data to tree.
     TreeNode *deserialize(const string &data) {
-
+        vector<string> strings;
+        for (int i = 1; i < data.size(); ++i) {
+            if (data[i] == ']' || data[i] == '[') {
+                continue;
+            }
+            int k = 0;
+            while (!(data[i + k] == ',' || data[i + k] == ']')) {
+                ++k;
+            }
+            strings.push_back(data.substr(i, k));
+            i += k;
+        }
+        queue<TreeNode *> q;
+        TreeNode *res;
+        if (strings.empty()) {
+            return nullptr;
+        }
+        if (strings[0] == "n") {
+            return nullptr;
+        } else {
+            res = new TreeNode(stoi(strings[0]));
+            q.push(res);
+        }
+        for (int i = 1; i < strings.size(); ++i) {
+            if (strings[i] == "n") {
+                q.front()->left = nullptr;
+            } else {
+                q.front()->left = new TreeNode(stoi(strings[i]));
+                q.push(q.front()->left);
+            }
+            ++i;
+            if (strings[i] == "n") {
+                q.front()->right = nullptr;
+            } else {
+                q.front()->right = new TreeNode(stoi(strings[i]));
+                q.push(q.front()->right);
+            }
+            q.pop();
+        }
+        return res;
     }
 };
 
