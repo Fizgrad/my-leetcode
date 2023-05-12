0d9ba8af7b45e1e5f4148cf18e997186d76f5e4f
Fizgrad <13272507567@163.com>
2023-05-12 11:55:17 +0800

    2140. Solving Questions With Brainpower

--- leetcode-cpp/src/Solution.cpp
+++ leetcode-cpp/src/Solution.cpp
@@ -4935,6 +4935,20 @@ public:
         }
         return dp[nums2.size()];
     }
+
+    long long mostPoints(vector<vector<int>> &questions) {
+        int n = questions.size();
+        vector<long long> dp(n, 0);
+        dp[n - 1] = questions[n - 1][0];
+        for (int i = n - 2; i >= 0; --i) {
+            if (i + questions[i][1] + 1 < n) {
+                dp[i] = max(dp[i + 1], questions[i][0] + dp[i + questions[i][1] + 1]);
+            } else {
+                dp[i] = max(dp[i + 1], (long long) questions[i][0]);
+            }
+        }
+        return dp[0];
+    }
 };
 
 int main() {
