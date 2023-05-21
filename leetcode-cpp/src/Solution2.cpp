9b857fbe945ad8e808d9d78cef060f6f308c7e2b
Fizgrad <13272507567@163.com>
2023-05-21 14:07:18 +0800

    934. Shortest Bridge

---
+++ leetcode-cpp/src/Solution2.cpp
@@ -0,0 +1,120 @@
+//
+// Created by David Chen on 5/20/23.
+//
+#include<iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <array>
+#include <set>
+#include <stack>
+#include <deque>
+#include <algorithm>
+#include <cmath>
+#include <cctype>
+#include <sstream>
+#include <regex>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <climits>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    explicit TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+    int shortestBridge(vector<vector<int>> &grid) {
+        int n = grid.size();
+        int xy[5] = {0, 1, 0, -1, 0};
+
+        auto isMargin = [&](int x, int y) -> bool {
+            for (int i = 0; i < 4; ++i) {
+                int xx = x + xy[i];
+                int yy = y + xy[i + 1];
+                if (xx >= 0 && yy >= 0 && xx < n && yy < n && grid[xx][yy] == 0) {
+                    return true;
+                }
+            }
+            return false;
+        };
+
+        vector<pair<int, int>> s;
+        vector<vector<bool>> visited(n, vector<bool>(n, false));
+        vector<pair<int, int>> margin;
+
+        bool flag = true;
+        for (int i = 0; i < n && flag; ++i) {
+            for (int j = 0; j < n && flag; ++j) {
+                if (grid[i][j] == 1) {
+                    s.emplace_back(i, j);
+                    visited[i][j] = true;
+                    flag = false;
+                }
+            }
+        }
+        while (!s.empty()) {
+            auto [x, y] = s.back();
+            s.pop_back();
+            if (isMargin(x, y)) {
+                margin.emplace_back(x, y);
+            }
+            for (int k = 0; k < 4; ++k) {
+                int xx = x + xy[k];
+                int yy = y + xy[k + 1];
+                if (xx >= 0 && yy >= 0 && xx < n && yy < n && grid[xx][yy] == 1 && !visited[xx][yy]) {
+                    visited[xx][yy] = true;
+                    s.emplace_back(xx, yy);
+                }
+            }
+        }
+        vector<pair<int, int>> temp;
+        vector<pair<int, int>> next(margin);
+        int res = 0;
+        while (!next.empty()) {
+            temp.clear();
+            for (auto [x, y]: next) {
+                for (int k = 0; k < 4; ++k) {
+                    int xx = x + xy[k];
+                    int yy = y + xy[k + 1];
+                    if (xx >= 0 && yy >= 0 && xx < n && yy < n) {
+                        if (grid[xx][yy] == 1 && !visited[xx][yy]) {
+                            return res;
+                        }
+                        if (grid[xx][yy] == 0 && !visited[xx][yy]) {
+                            visited[xx][yy] = true;
+                            temp.emplace_back(xx, yy);
+                        }
+                    }
+                }
+            }
+            next.swap(temp);
+            ++res;
+        }
+        return -1;
+    }
+
+};
+
+int main() {
+    Solution s;
+    return 0;
+}
