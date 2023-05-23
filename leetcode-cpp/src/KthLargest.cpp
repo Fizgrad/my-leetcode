31ba421d6ab7623098eb85abf38b44fc22a3bf06
Fizgrad <13272507567@163.com>
2023-05-23 10:49:49 +0800

    703. Kth Largest Element in a Stream

---
+++ leetcode-cpp/src/KthLargest.cpp
@@ -0,0 +1,32 @@
+//
+// Created by David Chen on 5/23/23.
+//
+#include<vector>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, less<int>> pq;
+    int k;
+
+    KthLargest(int k, vector<int> &nums) {
+        this->k = k;
+        for (auto i: nums) {
+            pq.push(-i);
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }
+    }
+
+    int add(int val) {
+        pq.push(-val);
+        if (pq.size() > k) {
+            pq.pop();
+        }
+        return -pq.top();
+    }
+};
