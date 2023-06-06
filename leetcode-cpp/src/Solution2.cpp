ad1c0ea7a6afc93fd2c9511b3fa0ac789fb4d9cc
Fizgrad <13272507567@163.com>
2023-06-06 13:32:02 +0800

    1502. Can Make Arithmetic Progression From Sequence

--- leetcode-cpp/src/Solution2.cpp
+++ leetcode-cpp/src/Solution2.cpp
@@ -502,6 +502,16 @@ class Solution {
         return true;
     }
 
+    bool canMakeArithmeticProgression(vector<int> &arr) {
+        std::sort(arr.begin(), arr.end());
+        for (int i = 1; i < arr.size(); ++i) {
+            if (arr[i] - arr[i - 1] != arr[1] - arr[0]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 };
 
 int main() {
