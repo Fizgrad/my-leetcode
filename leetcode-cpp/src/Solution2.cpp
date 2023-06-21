9a704a426f213782b8513248fd03f27dd169fd16
Fizgrad <13272507567@163.com>
2023-06-21 20:38:49 +0800

    2448. Minimum Cost to Make Array Equal

--- leetcode-cpp/src/Solution2.cpp
+++ leetcode-cpp/src/Solution2.cpp
@@ -945,7 +945,7 @@ class Solution {
         int n = nums.size();
         vector<pair<int, int>> vec;
         for (int i = 0; i < nums.size(); i++)
-            vec.push_back({nums[i], cost[i]});
+            vec.emplace_back(nums[i], cost[i]);
         sort(vec.begin(), vec.end());
         for (int i = 0; i < n; i++)
             sum += vec[i].second;
