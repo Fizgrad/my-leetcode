3d05ffd5b4902fda9168db615ad3a55f1b3007f9
Fizgrad <13272507567@163.com>
2023-07-27 11:59:43 +0800

    1870. Minimum Speed to Arrive on Time

--- leetcode-cpp/src/Solution2.cpp
+++ leetcode-cpp/src/Solution2.cpp
@@ -1867,6 +1867,41 @@ public:
         return low;
     }
 
+    int minSpeedOnTime(vector<int> &dist, double hour) {
+        if (dist.size() > ::ceil(hour)) {
+            return -1;
+        }
+        int high = INT32_MAX >> 1;
+        int low = 1;
+        auto f = [&](int speed) {
+            double ans = 0;
+            for (int i = 0; i < dist.size(); i++) {
+                double d = dist[i] * 1.0 / speed;
+                if (i != dist.size() - 1)
+                    ans = ans + ceil(d);
+                else
+                    ans += d;
+                if (ans > hour)
+                    return false;
+            }
+            if (ans <= hour)
+                return true;
+            return false;
+        };
+        int i = 1;
+        int j = 1e7;
+        int minspeed = -1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (f(mid)) {
+                minspeed = mid;
+                j = mid - 1;
+            } else
+                i = mid + 1;
+        }
+        return minspeed;
+    }
+
 };
 
 int main() {
