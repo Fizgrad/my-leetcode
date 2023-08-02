85cc9be4524a927a1056f1d3a9a365ee87995e55
Fizgrad <13272507567@163.com>
2023-08-02 15:10:20 +0800

    new solution3

---
+++ leetcode-cpp/src/Solution3.cpp
@@ -0,0 +1,51 @@
+//
+// Created by David Chen on 8/2/23.
+//
+#include<iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <array>
+#include <set>
+#include <stack>
+#include <deque>
+#include <algorithm>
+#include <cmath>
+#include <cctype>
+#include <sstream>
+#include <regex>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <climits>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    explicit TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+
+};
+
+int main() {
+    return 0;
+}
