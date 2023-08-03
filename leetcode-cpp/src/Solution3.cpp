6ec1e4de16602fc445d4dbc5e1453fb71507f8c6
Fizgrad <13272507567@163.com>
2023-08-03 19:49:11 +0800

    17. Letter Combinations of a Phone Number

--- leetcode-cpp/src/Solution3.cpp
+++ leetcode-cpp/src/Solution3.cpp
@@ -43,6 +43,34 @@ struct ListNode {
 
 class Solution {
 public:
+    vector<string> letterCombinations(const string &digits) {
+        if (digits.empty()) {
+            return {};
+        }
+        unordered_map<char, string> chars{{'1', ""},
+                                          {'2', "abc"},
+                                          {'3', "def"},
+                                          {'4', "ghi"},
+                                          {'5', "jkl"},
+                                          {'6', "mno"},
+                                          {'7', "pqrs"},
+                                          {'8', "tuv"},
+                                          {'9', "wxyz"},
+                                          {'0', ""}};
+        vector<string> next = {""};
+        vector<string> temp;
+        for (auto i: digits) {
+            for (auto c: chars[i]) {
+                for (auto k: next) {
+                    k.push_back(c);
+                    temp.emplace_back(k);
+                }
+            }
+            next.swap(temp);
+            temp.clear();
+        }
+        return next;
+    }
 
 };
 
