7234af89b998e96c26af51674e7c55ee603be1ba
fizgrad <13272507567@163.com>
2023-08-08 14:30:56 +0800

    33. Search in Rotated Sorted Array

--- leetcode-cpp/src/Solution3.cpp
+++ leetcode-cpp/src/Solution3.cpp
@@ -214,6 +214,43 @@ public:
         return solve(solve, n, goal, k);
     }
 
+    int search(vector<int> &nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
+        int mid;
+        int cut;
+        while (low <= high) {
+            mid = (low + high) >> 1;
+            if (nums[mid] >= nums[0]) {
+                cut = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        if (target == nums[0]) {
+            return 0;
+        } else if (target > nums[0]) {
+            low = 0;
+            high = cut;
+        } else {
+            low = cut + 1;
+            high = n - 1;
+        }
+        while (low <= high) {
+            mid = (low + high) >> 1;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
 };
 
 int main() {
