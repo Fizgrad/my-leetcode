71e668f87fbbc5dddb548ee5a07070d629581eae
Fizgrad <13272507567@163.com>
2023-08-13 01:22:40 +0800

    63. Unique Paths II

--- leetcode-cpp/src/Solution3.cpp
+++ leetcode-cpp/src/Solution3.cpp
@@ -366,6 +366,30 @@ public:
         return f(f, amount, 0);
     }
 
+    int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid) {
+        int n = obstacleGrid.size();
+        int m = obstacleGrid.begin()->size();
+        vector<int> res(m, 0);
+        vector<int> temp(m, 0);
+        for (int i = 0; i < m; ++i) {
+            if (obstacleGrid[0][i] == 0)
+                res[i] = 1;
+            else break;
+        }
+        while (--n) {
+            res.swap(temp);
+            if (obstacleGrid[obstacleGrid.size() - n][0] == 0) {
+                res[0] = temp[0];
+            } else res[0] = 0;
+            for (int i = 1; i < m; ++i) {
+                if (obstacleGrid[obstacleGrid.size() - n][i] == 0)
+                    res[i] = temp[i] + res[i - 1];
+                else res[i] = 0;
+            }
+        }
+        return res.back();
+    }
+
 };
 
 int main() {
