e19574572abc0b5d2a86773ef8463a105bd10444
Fizgrad <13272507567@163.com>
2023-09-01 10:39:04 +0800

    338. Counting Bits

--- leetcode-cpp/src/Solution3.cpp
+++ leetcode-cpp/src/Solution3.cpp
@@ -610,6 +610,14 @@ public:
         return res;
     }
 
+    vector<int> countBits(int n) {
+        vector<int> res(n + 1);
+        for (int i = 0; i < n + 1; ++i) {
+            res[i] = __builtin_popcount(i);
+        }
+        return res;
+    }
+
 };
 
 int main() {
