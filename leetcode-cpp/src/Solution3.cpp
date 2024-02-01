f6ddecd180c75586008b49d87a5bd1de89e25fde
fizgrad <13272507567@163.com>
2024-02-01 23:00:43 +0800

    2966. Divide Array Into Arrays With Max Difference

--- leetcode-cpp/src/Solution3.cpp
+++ leetcode-cpp/src/Solution3.cpp
@@ -1564,6 +1564,19 @@ public:
         return ans;
     }
 
+    vector<vector<int>> divideArray(vector<int> &nums, int k) {
+        std::sort(nums.begin(), nums.end());
+        vector<vector<int >> ans;
+        for (auto i = 0; i < nums.size(); i += 3) {
+            if (nums[i + 2] - nums[i] <= k) {
+                ans.push_back({nums[i], nums[i + 1], nums[i + 2]});
+            } else {
+                return {};
+            }
+        }
+        return ans;
+    }
+
 };
 
 int main() {
