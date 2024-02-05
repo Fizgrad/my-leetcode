842ca6e80d803aaea8bafc3278b87947ce7184a6
fizgrad <13272507567@163.com>
2024-02-05 17:35:29 +0800

    387. First Unique Character in a String

--- leetcode-cpp/src/Solution3.cpp
+++ leetcode-cpp/src/Solution3.cpp
@@ -1664,6 +1664,13 @@ public:
         return {res_start, res_end};
     }
 
+    int firstUniqChar(const std::string &s) {
+        int freq[26] = {};
+        for (char c: s) ++freq[c - 'a'];
+        for (int i = 0; i < s.size(); ++i) if (freq[s[i] - 'a'] == 1) return i;
+        return -1;
+    }
+
 };
 
 int main() {
