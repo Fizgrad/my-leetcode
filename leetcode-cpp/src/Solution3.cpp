93309859d212800a90cacbba5361f447dcec9067
fizgrad <13272507567@163.com>
2024-02-08 20:19:47 +0800

    279. Perfect Squares

--- leetcode-cpp/src/Solution3.cpp
+++ leetcode-cpp/src/Solution3.cpp
@@ -1727,6 +1727,25 @@ public:
         return std::move(res);
     }
 
+    int numSquares(int n) {
+        auto is_square = [](int n) {
+            int sqrt_n = (int) (sqrt(n));
+            return (sqrt_n * sqrt_n == n);
+        };
+        if (is_square(n)) return 1;
+        while ((n & 3) == 0) n >>= 2;
+        if ((n & 7) == 7) return 4;
+        int sqrt_n = (int) (sqrt(n));
+        for (int i = 1; i <= sqrt_n; i++) {
+            if (is_square(n - i * i)) {
+                return 2;
+            }
+        }
+        return 3;
+    }
+
+}
+
 };
 
 int main() {
