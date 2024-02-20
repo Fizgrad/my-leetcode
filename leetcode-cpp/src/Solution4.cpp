d5f1941b8847ccc1077f30a34613fc75e07b397e
fizgrad <13272507567@163.com>
2024-02-20 11:37:38 +0800

    268. Missing Number

--- leetcode-cpp/src/Solution4.cpp
+++ leetcode-cpp/src/Solution4.cpp
@@ -185,6 +185,15 @@ public:
         return n != -2147483648 && __builtin_popcount(n) == 1;
     }
 
+    int missingNumber(vector<int> &nums) {
+        int res = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            res ^= nums[i];
+            res ^= (i + 1);
+        }
+        return res;
+    }
+
 };
 
 int main() {
