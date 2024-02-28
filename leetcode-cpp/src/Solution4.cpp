5b702411805a48697737d56ff33c7efd0cffcd92
fizgrad <13272507567@163.com>
2024-02-28 11:41:43 +0800

    4. Median of Two Sorted Arrays

--- leetcode-cpp/src/Solution4.cpp
+++ leetcode-cpp/src/Solution4.cpp
@@ -392,6 +392,32 @@ class Solution {
         }
         return res;
     }
+
+    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        int i = 0;
+        int j = 0;
+        vector<int> nums(n + m, 0);
+        int k = 0;
+        while (i < n && j < m) {
+            if (nums1[i] > nums2[j]) {
+                nums[k++] = nums2[j++];
+            } else {
+                nums[k++] = nums1[i++];
+            }
+        }
+        while (i < n)
+            nums[k++] = nums1[i++];
+        while (j < m)
+            nums[k++] = nums2[j++];
+        bool even = k & 1;
+        if (even) {
+            return nums[(k - 1) >> 1];
+        } else {
+            return (static_cast<double>(nums[k >> 1]) + nums[(k >> 1) - 1]) / 2;
+        }
+    }
 };
 
 int main() { return 0; }
