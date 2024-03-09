b26b789269bd495604e047ab5d24110bd67e0985
david <david@davids-MacBook-Air.local>
2024-03-09 22:17:32 +0800

    2540. Minimum Common Value

--- leetcode-cpp/src/Solution4.cpp
+++ leetcode-cpp/src/Solution4.cpp
@@ -614,6 +614,18 @@ public:
         }
         return slow;
     }
+
+    int getCommon(vector<int> &nums1, vector<int> &nums2) {
+        int i = 0;
+        int j = 0;
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] == nums2[j]) return nums1[i];
+            if (nums1[i] > nums2[j]) ++j;
+            else
+                ++i;
+        }
+        return -1;
+    }
 };
 
 int main() { return 0; }
