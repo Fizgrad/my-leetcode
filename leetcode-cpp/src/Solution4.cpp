8df11237c1da6971e801265d653216a2df89e2d1
david <david@MateBook-Air.local>
2024-03-26 21:22:01 +0800

    41. First Missing Positive

--- leetcode-cpp/src/Solution4.cpp
+++ leetcode-cpp/src/Solution4.cpp
@@ -929,6 +929,28 @@ public:
         }
         return result;
     }
+
+    int firstMissingPositive(vector<int> &nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] <= 0) {
+                nums[i] = n + 100;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            int index = abs(nums[i]) - 1;
+            if (index >= n)
+                continue;
+            if (nums[index] > 0)
+                nums[index] = -nums[index];
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
 };
 
 int main() { return 0; }
