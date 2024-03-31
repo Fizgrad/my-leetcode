518e0e7a388300ffb836839dba8f3965f68e7eba
david <david@MateBook-Air.local>
2024-04-01 04:51:46 +0800

    2444. Count Subarrays With Fixed Bounds

--- leetcode-cpp/src/Solution4.cpp
+++ leetcode-cpp/src/Solution4.cpp
@@ -1033,6 +1033,28 @@ public:
         };
         return atMostKDistinct(nums, k) - atMostKDistinct(nums, k - 1);
     }
+
+    //  1   2    3    4    5   6   7   8
+    // bad good good min good max min good  <- now
+    // res += 4 - 1
+    // similar to subarray which elements are all equal to K
+    long long countSubarrays(vector<int> &nums, int minK, int maxK) {
+        long long res = 0;
+        int bad_idx = -1, left_idx = -1, right_idx = -1;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (!(minK <= nums[i] && nums[i] <= maxK)) {
+                bad_idx = i;
+            }
+            if (nums[i] == minK) {
+                left_idx = i;
+            }
+            if (nums[i] == maxK) {
+                right_idx = i;
+            }
+            res += max(0, min(left_idx, right_idx) - bad_idx);
+        }
+        return res;
+    }
 };
 
 int main() { return 0; }
