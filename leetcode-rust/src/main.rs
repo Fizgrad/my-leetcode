5f99e759eb6f6ec8327cde870cfdf38a0c41f389
david <david@MateBook-Air.local>
2024-04-18 00:14:07 +0800

    988. Smallest String Starting From Leaf

--- leetcode-rust/src/main.rs
+++ leetcode-rust/src/main.rs
@@ -1359,6 +1359,27 @@ impl Solution {
             root
         }
     }
+
+    pub fn smallest_from_leaf(root: Option<Rc<RefCell<TreeNode>>>) -> String {
+        fn dfs(root: &Option<Rc<RefCell<TreeNode>>>, mut path: String, ans: &mut String) {
+            if let Some(node) = root {
+                let node_ref = node.borrow();
+                let ch = char::from_u32((node_ref.val as u32) + ('a' as u32)).unwrap();
+                path.insert(0, ch);
+
+                if node_ref.left.is_none() && node_ref.right.is_none() {
+                    if ans.is_empty() || path < *ans {
+                        *ans = path.clone();
+                    }
+                }
+                dfs(&node_ref.left, path.clone(), ans);
+                dfs(&node_ref.right, path.clone(), ans);
+            }
+        }
+        let mut ans = String::new();
+        dfs(&root, String::new(), &mut ans);
+        ans
+    }
 }
 
 fn main() {
