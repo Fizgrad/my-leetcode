dcd3f114324af2c406d2832796ab9183a1a960bd
david <david@MateBook-Air.local>
2024-04-18 19:13:41 +0800

    463. Island Perimeter

--- leetcode-rust/src/main.rs
+++ leetcode-rust/src/main.rs
@@ -1380,6 +1380,20 @@ impl Solution {
         dfs(&root, String::new(), &mut ans);
         ans
     }
+
+    pub fn island_perimeter(grid: Vec<Vec<i32>>) -> i32 {
+        let mut res = 0;
+        for i in 0..grid.len() {
+            for j in 0..grid[0].len() {
+                if grid[i][j] == 1 {
+                    res += 4;
+                    if i > 0 && grid[i - 1][j] == 1 { res -= 2; }
+                    if j > 0 && grid[i][j - 1] == 1 { res -= 2; }
+                }
+            }
+        }
+        res
+    }
 }
 
 fn main() {
