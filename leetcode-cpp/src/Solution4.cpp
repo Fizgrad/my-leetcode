9584da6e716e35dcad8e3c8586c7c9a31c4b8bb6
fizgrad <13272507567@163.com>
2024-05-09 09:36:38 +0800

    3075. Maximize Happiness of Selected Children

--- leetcode-cpp/src/Solution4.cpp
+++ leetcode-cpp/src/Solution4.cpp
@@ -1669,6 +1669,15 @@ public:
         }
         return res;
     }
+
+    long long maximumHappinessSum(vector<int> &happiness, int k) {
+        sort(happiness.begin(), happiness.end(), greater<int>());
+        long long int res = 0;
+        for (int i = 0; i < k; i++) {
+            res += (max(0, happiness[i] - i));
+        }
+        return res;
+    }
 };
 
 int main() { return 0; }
