53bc2dc9ede2f61e0e9b870f6509c22de298b0b4
david <david@MateBook-Air.local>
2024-06-12 10:47:59 +0800

    75. Sort Colors

--- leetcode-cpp/src/Solution4.cpp
+++ leetcode-cpp/src/Solution4.cpp
@@ -2433,6 +2433,21 @@ public:
         std::sort(res.begin() + n, res.end());
         return res;
     }
+
+    void sortColors(vector<int> &nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.size() - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low++], nums[mid++]);
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums[mid], nums[high--]);
+            }
+        }
+    }
 };
 
 int main() { return 0; }
