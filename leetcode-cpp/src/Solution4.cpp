e5c256ffe9a6b55e0d528ac2192966db3d618460
fizgrad <davidchenms@hotmail.com>
2024-07-03 13:50:54 +0800

    1509. Minimum Difference Between Largest and Smallest Value in Three Moves

--- leetcode-cpp/src/Solution4.cpp
+++ leetcode-cpp/src/Solution4.cpp
@@ -2880,6 +2880,15 @@ public:
         for (auto degree: degrees) res += degree * (++value);
         return res;
     }
+
+    int minDifference(vector<int> &nums) {
+        int n = nums.size();
+        if (n <= 4) {
+            return 0;
+        }
+        std::sort(nums.begin(), nums.end());
+        return std::min(std::min(std::min(nums[n - 4] - nums[0], nums[n - 3] - nums[1]), nums[n - 2] - nums[2]), nums[n - 1] - nums[3]);
+    }
 };
 
 int main() {
