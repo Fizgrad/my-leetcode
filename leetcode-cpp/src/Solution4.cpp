252f38bd4dcda55905baffe12e58b3a490bea7ae
fizgrad <davidchenms@hotmail.com>
2024-07-09 14:18:06 +0800

    2582. Pass the Pillow

--- leetcode-cpp/src/Solution4.cpp
+++ leetcode-cpp/src/Solution4.cpp
@@ -2939,6 +2939,11 @@ public:
         }
         return res / n;
     }
+
+    int passThePillow(int n, int time) {
+        int chunks = time / (n - 1);
+        return chunks % 2 == 0 ? (time % (n - 1) + 1) : (n - time % (n - 1));
+    }
 };
 
 int main() {
