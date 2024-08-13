1245ced50d150a6ac8f75f99cbe27a46b18f61e0
fizgrad <davidchenms@hotmail.com>
2024-08-13 17:00:18 +0800

    40. Combination Sum II

--- leetcode-cpp/src/Solution4.cpp
+++ leetcode-cpp/src/Solution4.cpp
@@ -3295,6 +3295,34 @@ public:
 
         return result;
     }
+
+    vector<vector<int>> combinationSum2(vector<int> &candidates, int target) {
+        int n = candidates.size();
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> result;
+        vector<int> temp;
+        int sum = 0;
+        auto dfs = [&](auto &&dfs, int index) -> void {
+            if (index >= n)
+                return;
+            for (int i = index; i < n; i++) {
+                if (i > index && candidates[i] == candidates[i - 1]) continue;
+                if (candidates[i] > target) break;
+                temp.push_back(candidates[i]);
+                sum += candidates[i];
+                if (sum == target) {
+                    result.push_back(temp);
+                }
+                if (sum < target) {
+                    dfs(dfs, i + 1);
+                }
+                temp.pop_back();
+                sum -= candidates[i];
+            }
+        };
+        dfs(dfs, 0);
+        return result;
+    }
 };
 
 int main() {
