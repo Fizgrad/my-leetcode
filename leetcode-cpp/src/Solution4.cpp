a3b565fd4192e833aef0948fe5f138f751e5437a
fizgrad <davidchenms@hotmail.com>
2024-09-19 14:45:48 +0800

    241. Different Ways to Add Parentheses

--- leetcode-cpp/src/Solution4.cpp
+++ leetcode-cpp/src/Solution4.cpp
@@ -3819,6 +3819,28 @@ public:
         }
         return res;
     }
+
+    vector<int> diffWaysToCompute(const string &expression) {
+        vector<int> res;
+        for (int i = 0; i < expression.size(); ++i) {
+            char oper = expression[i];
+            if (oper == '+' || oper == '-' || oper == '*') {
+                vector<int> s1 = diffWaysToCompute(expression.substr(0, i));
+                vector<int> s2 = diffWaysToCompute(expression.substr(i + 1));
+                for (int a: s1) {
+                    for (int b: s2) {
+                        if (oper == '+') res.push_back(a + b);
+                        else if (oper == '-')
+                            res.push_back(a - b);
+                        else if (oper == '*')
+                            res.push_back(a * b);
+                    }
+                }
+            }
+        }
+        if (res.empty()) res.push_back(stoi(expression));
+        return res;
+    }
 };
 
 int main() {
