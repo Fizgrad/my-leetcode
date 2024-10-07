6cc1edb6966ac21c51f98932d31b4a03c899c1c6
fizgrad <davidchenms@hotmail.com>
2024-10-07 10:13:18 +0800

    2696. Minimum String Length After Removing Substrings

--- leetcode-cpp/src/Solution4.cpp
+++ leetcode-cpp/src/Solution4.cpp
@@ -4095,6 +4095,25 @@ public:
         }
         return totalChemistry / 2;
     }
+
+    int minLength(const string &s) {
+        stack<char> stack;
+        for (int i = 0; i < s.length(); i++) {
+            char cur_char = s[i];
+            if (stack.empty()) {
+                stack.push(cur_char);
+                continue;
+            }
+            if (cur_char == 'B' && stack.top() == 'A') {
+                stack.pop();
+            } else if (cur_char == 'D' && stack.top() == 'C') {
+                stack.pop();
+            } else {
+                stack.push(cur_char);
+            }
+        }
+        return stack.size();
+    }
 };
 
 int main() {
