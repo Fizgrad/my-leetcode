ebb2f073e25b5c65b3d3c4c7356e8729a3f61b4b
fizgrad <davidchenms@hotmail.com>
2024-10-24 15:45:26 +0800

    951. Flip Equivalent Binary Trees

--- leetcode-cpp/src/Solution4.cpp
+++ leetcode-cpp/src/Solution4.cpp
@@ -4439,6 +4439,20 @@ public:
         }
         return root;
     }
+
+    bool flipEquiv(TreeNode *root1, TreeNode *root2) {
+        if (root1 == nullptr && root2 == nullptr) {
+            return true;
+        }
+        if (root1 == nullptr || root2 == nullptr) {
+            return false;
+        }
+        if (root1->val == root2->val) {
+            return flipEquiv(root1->left, root2->right) && flipEquiv(root1->right, root2->left) || flipEquiv(root1->left, root2->left) && flipEquiv(root1->right, root2->right);
+        } else {
+            return false;
+        }
+    }
 };
 
 int main() {
