75b2f0949c610f306691151161913f75a0c3860c
Fizgrad <davidchenms@hotmail.com>
2025-03-01 16:18:25 +0800

    2460. Apply Operations to an Array

--- leetcode-cpp/src/Solution3.cpp
+++ leetcode-cpp/src/Solution3.cpp
@@ -2992,6 +2992,23 @@ public:
         }
         return res;
     }
+
+    vector<int> applyOperations(vector<int> &nums) {
+        int n = nums.size();
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] == nums[i - 1]) {
+                nums[i - 1] *= 2;
+                nums[i] = 0;
+            }
+        }
+        int index = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] != 0) {
+                std::swap(nums[i], nums[index++]);
+            }
+        }
+        return nums;
+    }
 };
 
 int main() {
