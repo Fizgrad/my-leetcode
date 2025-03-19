9edbe19c319e816cb296b2609e03d9997b5582bf
Fizgrad <davidchenms@hotmail.com>
2025-03-19 14:15:45 +0800

    3191. Minimum Operations to Make Binary Array Elements Equal to One I

--- leetcode-cpp/src/Solution2.cpp
+++ leetcode-cpp/src/Solution2.cpp
@@ -3825,6 +3825,23 @@ public:
         }
         return static_cast<long long>(nums.size()) * (nums.size() - 1) / 2 - res;
     }
+
+    int minOperations(vector<int> &nums) {
+        int n = nums.size();
+        int res = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == 0) {
+                if (i + 2 >= n) return -1;
+                else {
+                    nums[i] = 1 - nums[i];
+                    nums[i + 1] = 1 - nums[i + 1];
+                    nums[i + 2] = 1 - nums[i + 2];
+                    ++res;
+                }
+            }
+        }
+        return res;
+    }
 };
 
 int main() {
