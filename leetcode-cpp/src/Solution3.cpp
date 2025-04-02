72d1b6fb36f8b71d6fa2cc1ddc89e43b4e0222bc
Fizgrad <davidchenms@hotmail.com>
2025-04-02 10:53:52 +0800

    2873. Maximum Value of an Ordered Triplet I

--- leetcode-cpp/src/Solution3.cpp
+++ leetcode-cpp/src/Solution3.cpp
@@ -3537,6 +3537,19 @@ public:
         }
         return dp[0];
     }
+
+    long long maximumTripletValue(vector<int> &nums) {
+        int n = nums.size();
+        long long res = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                for (int z = j + 1; z < n; ++z) {
+                    res = max(res, static_cast<long long>(nums[z]) * (nums[i] - nums[j]));
+                }
+            }
+        }
+        return res;
+    }
 };
 
 int main() {
