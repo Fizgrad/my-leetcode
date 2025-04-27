599637ced10ce00d6c34f834209a45dcb601c15a
Fizgrad <davidchenms@hotmail.com>
2025-04-27 10:52:15 +0800

    3392. Count Subarrays of Length Three With a Condition

--- leetcode-cpp/src/Solution3.cpp
+++ leetcode-cpp/src/Solution3.cpp
@@ -3820,6 +3820,15 @@ public:
         }
         return res;
     }
+
+    int countSubarrays(vector<int> &nums) {
+        int res = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i + 2 >= nums.size()) return res;
+            if ((nums[i] + nums[i + 2]) * 2 == nums[i + 1]) ++res;
+        }
+        return res;
+    }
 };
 
 int main() {
