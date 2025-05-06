aa8ff729ffc73e13f6e48f5f9a51e7e6e5c3689f
Fizgrad <davidchenms@hotmail.com>
2025-05-06 14:05:38 +0800

    1920. Build Array from Permutation

--- leetcode-cpp/src/Solution3.cpp
+++ leetcode-cpp/src/Solution3.cpp
@@ -4051,6 +4051,14 @@ public:
         }
         return res;
     }
+
+    vector<int> buildArray(vector<int> &nums) {
+        vector<int> res(nums.size());
+        for (int i = 0; i < nums.size(); ++i) {
+            res[i] = nums[nums[i]];
+        }
+        return res;
+    }
 };
 
 int main() {
