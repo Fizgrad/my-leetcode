9f9ab4604f148f98ced09437a50d22be255b706b
Fizgrad <davidchenms@hotmail.com>
2025-05-21 12:08:42 +0800

    73. Set Matrix Zeroes

--- leetcode-cpp/src/Solution3.cpp
+++ leetcode-cpp/src/Solution3.cpp
@@ -4333,6 +4333,58 @@ public:
         }
         return std::accumulate(counts.begin(), counts.end(), 0ll) % MOD;
     }
+
+    void setZeroes(vector<vector<int>> &matrix) {
+        int m = matrix.size();
+        int n = matrix.front().size();
+        bool firstCol = false;
+        bool firstRow = false;
+        for (int i = 0; i < m; ++i) {
+            if (matrix[i][0] == 0) {
+                firstCol = true;
+            }
+        }
+        for (int j = 0; j < n; ++j) {
+            if (matrix[0][j] == 0) {
+                firstRow = true;
+            }
+        }
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < m; ++i) {
+            if (matrix[i][0] == 0) {
+                for (int j = 0; j < n; ++j) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for (int j = 1; j < n; ++j) {
+            if (matrix[0][j] == 0) {
+                for (int i = 0; i < m; ++i) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (firstCol) {
+            for (int i = 0; i < m; ++i) {
+                matrix[i][0] = 0;
+            }
+        }
+        if (firstRow) {
+            for (int j = 0; j < n; ++j) {
+                matrix[0][j] = 0;
+            }
+        }
+    }
 };
 
 int main() {
