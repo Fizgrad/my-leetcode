922424b7d14326c463f889e384548563ec2e6e29
Fizgrad <davidchenms@hotmail.com>
2025-06-13 13:15:19 +0800

    2616. Minimize the Maximum Difference of Pairs

--- leetcode-cpp/src/Solution2.cpp
+++ leetcode-cpp/src/Solution2.cpp
@@ -4857,6 +4857,34 @@ public:
         }
         return res;
     }
+
+    int minimizeMax(vector<int> &nums, int p) {
+        sort(nums.begin(), nums.end());
+        int low = 0;
+        int high = nums.back() - nums.front();
+        int res = 0;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if ([&](int p, int maxDiff) -> bool {
+                    int count = 0;
+                    for (int i = 1; i < nums.size() && count < p;) {
+                        if (nums[i] - nums[i - 1] <= maxDiff) {
+                            count++;
+                            i += 2;
+                        } else {
+                            i++;
+                        }
+                    }
+                    return count >= p;
+                }(p, mid)) {
+                high = mid - 1;
+                res = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return res;
+    }
 };
 
 int main() {
