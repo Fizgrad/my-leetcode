118031c987abab009ce8654439bd171c3e604809
david <davidchenms@hotmail.com>
2025-08-05 10:00:22 +0800

    3477. Fruits Into Baskets II

--- leetcode-cpp/src/Solution4.cpp
+++ leetcode-cpp/src/Solution4.cpp
@@ -5347,6 +5347,20 @@ public:
         }
         return res;
     }
+
+    int numOfUnplacedFruits(vector<int> &fruits, vector<int> &baskets) {
+        int place = 0;
+        for (int i = 0; i < fruits.size(); ++i) {
+            for (int j = 0; j < baskets.size(); ++j) {
+                if (baskets[j] >= fruits[i]) {
+                    baskets[j] = 0;
+                    ++place;
+                    break;
+                }
+            }
+        }
+        return fruits.size() - place;
+    }
 };
 
 int main() {
