34a694af65b8a44f7f76a6f8a697f2dbc0ec1069
david <davidchenms@hotmail.com>
2025-08-09 23:32:59 +0800

    231. Power of Two

--- leetcode-cpp/src/Solution5.cpp
+++ leetcode-cpp/src/Solution5.cpp
@@ -174,6 +174,10 @@ public:
         res += dp[n - 1][n - 1];
         return res;
     }
+
+    bool isPowerOfTwo(int n) {
+        return n != -2147483648 && __builtin_popcount(n) == 1;
+    }
 };
 
 int main() {
