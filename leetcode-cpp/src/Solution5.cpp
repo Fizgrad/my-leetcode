75a9cc6a3e8db77e51694fad661d23a65ac2da85
david <davidchenms@hotmail.com>
2025-08-15 13:17:23 +0800

    342. Power of Four

--- leetcode-cpp/src/Solution5.cpp
+++ leetcode-cpp/src/Solution5.cpp
@@ -279,6 +279,10 @@ public:
         }
         return res;
     }
+
+    bool isPowerOfFour(int n) {
+        return n > 0 && __builtin_popcount(n) == 1 && (n - 1) % 3 == 0;
+    }
 };
 
 int main() {
