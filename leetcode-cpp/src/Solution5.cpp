da391b04e9c24b86ca3a707eee4d83412d95d333
david <davidchenms@hotmail.com>
2025-09-26 10:00:17 +0800

    611. Valid Triangle Number

--- leetcode-cpp/src/Solution5.cpp
+++ leetcode-cpp/src/Solution5.cpp
@@ -1238,6 +1238,25 @@ public:
         }
         return *std::min_element(dp.begin(), dp.end());
     }
+
+    int triangleNumber(vector<int> &nums) {
+        int n = nums.size();
+        std::sort(nums.begin(), nums.end());
+        int res = 0;
+        for (int i = n - 1; i >= 2; --i) {
+            int left = 0;
+            int right = i - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] > nums[i]) {
+                    res += (right - left);
+                    --right;
+                } else {
+                    ++left;
+                }
+            }
+        }
+        return res;
+    }
 };
 
 int main() {
