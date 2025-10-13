d032c7059555f43a23360d6880ec8ef84c688f3a
david <davidchenms@hotmail.com>
2025-10-13 11:48:51 +0800

    3539. Find Sum of Array Product of Magical Sequences

--- leetcode-cpp/src/Solution5.cpp
+++ leetcode-cpp/src/Solution5.cpp
@@ -1524,6 +1524,59 @@ public:
         }
         return res;
     }
+
+    int magicalSum(int m, int k, vector<int> &nums) {
+        constexpr int MOD = 1e9 + 7;
+        int C[31][31] = {{0}};
+        int n = nums.size();
+        unordered_map<int, int> dp;
+        auto modPow = [&](long long x, unsigned exp, int mod = MOD) -> long long {
+            if (exp == 0) return 1;
+            const int bM = 31 - countl_zero(exp);
+            bitset<32> B(exp);
+            long long y = x;
+            for (int b = bM - 1; b >= 0; b--)
+                y = (y * y % mod) * (B[b] ? x : 1) % mod;
+            return y;
+        };
+        auto Pascal = [&]() {
+            if (C[0][0] == 1) return;
+            for (int i = 1; i <= 30; i++) {
+                C[i][0] = C[i][i] = 1;
+                for (int j = 1; j <= i / 2; j++) {
+                    const int Cij = C[i - 1][j - 1] + C[i - 1][j];
+                    C[i][j] = C[i][i - j] = Cij;
+                }
+            }
+        };
+        auto dfs = [&](auto &&dfs, int m, int k, int i, unsigned flag, vector<int> &nums) {
+            const int bz = popcount(flag);
+            if (m < 0 || k < 0 || m + bz < k)
+                return 0;
+            if (m == 0)
+                return (k == bz) ? 1 : 0;
+            if (i >= n) return 0;
+
+            uint64_t key = (m << 5) | (k << 10) | (i << 16) | flag;
+            auto it = dp.find(key);
+            if (it != dp.end()) return it->second;
+
+            long long ans = 0;
+            for (int f = 0; f <= m; f++) {
+                long long perm = C[m][f] * modPow(nums[i], f) % MOD;
+
+                unsigned newFlag = flag + f;
+                unsigned nextFlag = newFlag >> 1;
+                bool bitContribution = newFlag & 1;
+
+                ans = (ans + perm * dfs(dfs, m - f, k - bitContribution, i + 1, nextFlag, nums)) % MOD;
+            }
+
+            return dp[key] = ans;
+        };
+        Pascal();
+        return dfs(dfs, m, k, 0, 0, nums);
+    }
 };
 
 int main() {
