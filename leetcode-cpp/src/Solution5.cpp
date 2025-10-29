a53c7346fdda0f655d9dc75ffb900822dbf7b0bd
david <davidchenms@hotmail.com>
2025-10-29 11:35:31 +0800

    3370. Smallest Number With All Set Bits

--- leetcode-cpp/src/Solution5.cpp
+++ leetcode-cpp/src/Solution5.cpp
@@ -1827,6 +1827,14 @@ public:
         }
         return res;
     }
+
+    int smallestNumber(int n) {
+        for (int i = 0; i < 32; ++i) {
+            if (((1 << i) - 1) >= n)
+                return ((1 << i) - 1);
+        }
+        return ((1 << 10) - 1);
+    }
 };
 
 int main() {
