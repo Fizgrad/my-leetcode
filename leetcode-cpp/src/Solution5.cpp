0f93141846d4f6f601b20f1331612241f37fc1b0
david <davidchenms@hotmail.com>
2025-11-21 10:51:15 +0800

    757. Set Intersection Size At Least Two

--- leetcode-cpp/src/Solution5.cpp
+++ leetcode-cpp/src/Solution5.cpp
@@ -2193,6 +2193,33 @@ public:
         }
         return res;
     }
+
+    int intersectionSizeTwo(vector<vector<int>> &intervals) {
+        int n = intervals.size();
+        std::sort(intervals.begin(), intervals.end(), [](const auto &a, const auto &b) {
+            return a[1] != b[1] ? a[1] < b[1] : a[0] > b[0];
+        });
+        int res = 0;
+        int first = -1;
+        int second = -1;
+        for (const auto &interval: intervals) {
+            int start = interval[0];
+            int end = interval[1];
+            if (start <= first) {
+                continue;
+            }
+            if (start > second) {
+                res += 2;
+                second = end;
+                first = end - 1;
+            } else {
+                res += 1;
+                first = second;
+                second = end;
+            }
+        }
+        return res;
+    }
 };
 
 int main() {
