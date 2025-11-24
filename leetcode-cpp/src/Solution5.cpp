5d8abe55f70dd93223c96612d38804f95b1eed6b
david <davidchenms@hotmail.com>
2025-11-24 15:06:07 +0800

    1018. Binary Prefix Divisible By 5

--- leetcode-cpp/src/Solution5.cpp
+++ leetcode-cpp/src/Solution5.cpp
@@ -2261,6 +2261,17 @@ public:
             return remove == INT_MAX ? 0 : sum - remove;
         }
     }
+
+    vector<bool> prefixesDivBy5(vector<int> &nums) {
+        int n = nums.size();
+        vector<bool> res(n, false);
+        int prefix = 0;
+        for (int i = 0; i < n; ++i) {
+            prefix = ((prefix << 1) + nums[i]) % 5;
+            res[i] = (prefix == 0);
+        }
+        return res;
+    }
 };
 
 int main() {
