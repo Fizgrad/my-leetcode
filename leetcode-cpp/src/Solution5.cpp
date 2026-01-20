7bf6dbc9bf86dc7d2e13c8c048bc56fc14578a30
david <davidchenms@hotmail.com>
2026-01-21 02:34:44 +0800

    3314. Construct the Minimum Bitwise Array I

--- leetcode-cpp/src/Solution5.cpp
+++ leetcode-cpp/src/Solution5.cpp
@@ -3554,6 +3554,20 @@ public:
         }
         return res;
     }
+
+    vector<int> minBitwiseArray(vector<int> &nums) {
+        auto n = nums.size();
+        vector<int> res(n, -1);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < nums[i]; ++j) {
+                if ((j | (j + 1)) == nums[i]) {
+                    res[i] = j;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
 };
 
 int main() {
