aa7724beecbd28277393d640b3bf9152fd15bb34
david <davidchenms@hotmail.com>
2026-02-20 14:16:32 +0800

    761. Special Binary String

--- leetcode-cpp/src/Solution5.cpp
+++ leetcode-cpp/src/Solution5.cpp
@@ -4190,6 +4190,27 @@ public:
         res += min(prev, now);
         return res;
     }
+
+    string makeLargestSpecial(string s) {
+        int count = 0, i = 0;
+        vector<string> res;
+        for (int j = 0; j < s.size(); ++j) {
+            count += (s[j] == '1' ? 1 : -1);
+            if (count == 0) {
+                // 剥离掉首尾的 1 和 0，对内部进行递归处理
+                string inner = makeLargestSpecial(s.substr(i + 1, j - i - 1));
+                // 重新组装并存入数组
+                res.push_back('1' + inner + '0');
+                i = j + 1;
+            }
+        }
+        // 将同层级的特殊子串降序排列
+        sort(res.rbegin(), res.rend());
+
+        string ans = "";
+        for (const string &str: res) ans += str;
+        return ans;
+    }
 };
 
 int main() {
