a1b79fd9c9396713c464bb50107b0aa2e30b38fe
david <davidchenms@hotmail.com>
2026-05-22 10:51:21 +0800

    33. Search in Rotated Sorted Array

--- leetcode-cpp/src/Solution5.cpp
+++ leetcode-cpp/src/Solution5.cpp
@@ -5833,6 +5833,51 @@ public:
         }
         return res;
     }
+
+    int search(vector<int> &nums, int target) {
+        int n = nums.size();
+        int right = n - 1;
+        int left = 0;
+        int mid = (left + right) >> 1;
+
+        while (left <= right) {
+            mid = (left + right) >> 1;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[left] == target) return left;
+            if (nums[right] == target) return right;
+            if (nums[right] > nums[left]) {
+                if (nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                if (nums[mid] > nums[left]) {
+                    if (target < nums[left]) {
+                        left = mid + 1;
+                    } else {
+                        if (nums[mid] < target) {
+                            left = mid + 1;
+                        } else {
+                            right = mid - 1;
+                        }
+                    }
+                } else {
+                    if (target < nums[right]) {
+                        if (nums[mid] > target) {
+                            right = mid - 1;
+                        } else {
+                            left = mid + 1;
+                        }
+                    } else {
+                        right = mid - 1;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
 };
 
 int main() {
