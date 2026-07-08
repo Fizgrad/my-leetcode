6bc5f30a4923f2ff40ad05e1e0a637b9a6379968
david <davidchenms@hotmail.com>
2026-07-09 02:14:40 +0800

    3756. Concatenate Non-Zero Digits and Multiply by Sum II

--- leetcode-cpp/src/Solution5.cpp
+++ leetcode-cpp/src/Solution5.cpp
@@ -6475,6 +6475,52 @@ public:
         }
         return stoll(num) * sum;
     }
+
+    vector<int> sumAndMultiply(const string &s, vector<vector<int>> &queries) {
+        int n = s.size();
+        constexpr int MOD = 1e9 + 7;
+        vector<int> sum_prefix(n + 1, 0);
+        for (int i = 0; i < n; ++i) {
+            sum_prefix[i + 1] = sum_prefix[i] + s[i] - '0';
+        }
+        vector<int> digits;
+        vector<int> pos;
+        for (int i = 0; i < n; ++i) {
+            if (s[i] != '0') {
+                digits.push_back(s[i] - '0');
+                pos.push_back(i);
+            }
+        }
+        int m = digits.size();
+        vector<long long> num_prefix(m + 1, 0);
+        vector<long long> pow10(m + 1, 1);
+
+        for (int i = 0; i < m; ++i) {
+            num_prefix[i + 1] = (num_prefix[i] * 10 + digits[i]) % MOD;
+            pow10[i + 1] = pow10[i] * 10 % MOD;
+        }
+
+        vector<int> ans;
+        for (auto &q: queries) {
+            int l = q[0];
+            int r = q[1];
+            long long sum = sum_prefix[r + 1] - sum_prefix[l];
+            // 找非0数字范围
+            int left = lower_bound(pos.begin(), pos.end(), l) - pos.begin();
+            int right = upper_bound(pos.begin(), pos.end(), r) - pos.begin() - 1;
+            long long x = 0;
+            if (left <= right) {
+                int len = right - left + 1;
+                x = (num_prefix[right + 1] -
+                     num_prefix[left] * pow10[len]) %
+                    MOD;
+                if (x < 0)
+                    x += MOD;
+            }
+            ans.push_back(x * (sum % MOD) % MOD);
+        }
+        return ans;
+    }
 };
 
 int main() {
