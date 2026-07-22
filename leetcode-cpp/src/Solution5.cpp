6f6299fe68f6d7d1262bc8e2956f28ede31afb79
Fizgrad <davidchenms@hotmail.com>
2026-07-22 10:51:00 +0800

    128. Longest Consecutive Sequence

--- leetcode-cpp/src/Solution5.cpp
+++ leetcode-cpp/src/Solution5.cpp
@@ -6755,6 +6755,23 @@ public:
         }
         return res;
     }
+
+    int longestConsecutive(vector<int> &nums) {
+        unordered_set<int> numSet(nums.begin(), nums.end());
+        int longestStreak = 0;
+        for (int num: numSet) {
+            if (!numSet.count(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+                while (numSet.count(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+                longestStreak = max(longestStreak, currentStreak);
+            }
+        }
+        return longestStreak;
+    }
 };
 
 int main() {
