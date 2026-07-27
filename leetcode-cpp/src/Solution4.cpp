d6d915486f61fd3ffc48184446d2db5251e01cad
Fizgrad <davidchenms@hotmail.com>
2026-07-27 09:16:37 +0800

    1464. Maximum Product of Two Elements in an Array

--- leetcode-cpp/src/Solution4.cpp
+++ leetcode-cpp/src/Solution4.cpp
@@ -6799,6 +6799,20 @@ public:
         const unsigned n = nums.size();
         return (n < 3) ? n : 1 << (32 - countl_zero(n));
     }
+
+    int maxProduct(vector<int> &nums) {
+        int n = nums.size();
+        int max1 = INT_MIN, max2 = INT_MIN;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > max1) {
+                max2 = max1;
+                max1 = nums[i];
+            } else if (nums[i] > max2) {
+                max2 = nums[i];
+            }
+        }
+        return (max1 - 1) * (max2 - 1);
+    }
 };
 
 int main() {
