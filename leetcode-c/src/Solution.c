452ce131a07e5ffb0fa9b232a786f582bb99a9b7
Fizgrad <davidchenms@hotmail.com>
2026-08-03 11:15:36 +0800

    1406. Stone Game III

--- leetcode-c/src/Solution.c
+++ leetcode-c/src/Solution.c
@@ -1,6 +1,7 @@
+#include <limits.h>
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <math.h>
 
 bool predictTheWinner(int *nums, int numsSize) {
     int dp[20][20] = {0};
@@ -14,6 +15,39 @@ bool predictTheWinner(int *nums, int numsSize) {
         }
     }
     return dp[0][numsSize - 1] >= 0;
+}
+
+bool stoneGame(int *piles, int pilesSize) {
+    int dp[505][505] = {0};
+    for (int i = 0; i < pilesSize; i++) {
+        dp[i][i] = piles[i];
+    }
+    for (int len = 2; len <= pilesSize; ++len) {
+        for (int i = 0; i + len - 1 < pilesSize; ++i) {
+            int j = i + len - 1;
+            dp[i][j] = fmax(piles[i] - dp[i + 1][j], piles[i] - dp[i][j - 1]);
+        }
+    }
+    return dp[0][pilesSize - 1] > 0;
+}
+
+char* stoneGameIII(int* stoneValue, int stoneValueSize) {
+    int dp[50005] = {0};
+    for (int i = stoneValueSize - 1; i >= 0; --i) {
+        dp[i] = INT_MIN;
+        int sum = 0;
+        for (int k = 0; k < 3 && i + k < stoneValueSize; ++k) {
+            sum += stoneValue[i + k];
+            dp[i] = fmax(dp[i], sum - dp[i + k + 1]);
+        }
+    }
+    if (dp[0] > 0) {
+        return "Alice";
+    } else if (dp[0] < 0) {
+        return "Bob";
+    } else {
+        return "Tie";
+    }   
 }
 
 int main() {
